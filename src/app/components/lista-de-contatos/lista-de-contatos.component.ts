26ad7cbc40807cd1c87214d7ef73fa8cca6cf07e
Brenno Henrique <henri.brenno@gmail.com>
2021-08-30 12:07:11 -0300

    deixando o contato mais dinamico

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  contatos:Contato[] = [
+    {
+      nome:"Smith Weber Jagerr Man Jansen",
+      email: "thenumberone@gmail.com",
+      telefones:['1111-1111']
+    },
+    {
+      nome:"Ms Puff",
+      email: "driverlicense@gmail.com",
+      telefones:['2222-2222']
+    },
+    {
+      nome:"Sr Krabby",
+      email: "krabby@gmail.com",
+      telefones:['3333-3333']
+    },
+    {
+      nome:"Sponge Bob",
+      email: "jellyfish@gmail.com",
+      telefones:['4444-4444']
+    },
+    {
+      nome:"Patrick",
+      email: "starfish@gmail.com",
+      telefones:['5555-5555']
+    },
+    {
+      nome:"Smith Weber Jagerr Man Jansen",
+      email: "thenumberone@gmail.com",
+      telefones:['1111-1111']
+    },
+    {
+      nome:"Smith Weber Jagerr Man Jansen",
+      email: "thenumberone@gmail.com",
+      telefones:['1111-1111']
+    },
+    {
+      nome:"Smith Weber Jagerr Man Jansen",
+      email: "thenumberone@gmail.com",
+      telefones:['1111-1111']
+    },
+    {
+      nome:"Smith Weber Jagerr Man Jansen",
+      email: "thenumberone@gmail.com",
+      telefones:['1111-1111']
+    },
+    {
+      nome:"Smith Weber Jagerr Man Jansen",
+      email: "thenumberone@gmail.com",
+      telefones:['1111-1111']
+    }
+  ]
 
   constructor() { }
 
